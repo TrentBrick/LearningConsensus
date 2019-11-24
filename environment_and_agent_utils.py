0bc588d33d16c7386a427326cfa9747c9f028395
Rohan Reddy <rr195@duke.edu>
2019-11-24 17:14:21 -0500

    changing action spaces

--- environment_and_agent_utils.py
+++ environment_and_agent_utils.py
@@ -1,4 +1,4 @@
-import numpy as np 
+import numpy as np
 import torch
 #from config import *
 from nn import *
@@ -62,8 +62,9 @@ def getActionSpace(params, isByzantine, byzantine_inds=None, can_send_either_val
 
         else:
             if can_send_either_value:
+				action_space.append('sample_k_avalanche')
                 for commit_val in params['commit_vals']:
-                    action_space.append('send_to_all-value_'+str(commit_val))
+                	#action_space.append('send_to_all-value_'+str(commit_val))
                     action_space.append('commit_'+str(commit_val))
             else:
                 action_space.append('send_to_all-value_init')
@@ -80,19 +81,19 @@ def toOneHotState(state, oneHotStateMapper, device):
         oh.append(oneHotStateMapper[s, :])
     oh = np.asarray(oh).flatten().T # now each column is one of the states.
     #print("one hot", oh, oh.shape)
-    #convert ot pytorch tensor: 
+    #convert ot pytorch tensor:
     #print('the resulting one hot', oh)
     oh = torch.from_numpy(oh).float().to(device)
     return oh
 
 def toOneHotActions(isByz, action_ind, byz_oneHotActionMapper, honest_oneHotActionMapper, device):
     # convert the action into an action ind:
-    if isByz: 
+    if isByz:
         oh = byz_oneHotActionMapper[action_ind, :]
-    else: 
+    else:
         oh = honest_oneHotActionMapper[action_ind, :]
     oh = torch.from_numpy(oh).float().to(device)
-    return oh 
+    return oh
 
 class Agent:
     def __init__(self, params, honest_policy, byz_policy, isByzantine, agentID, byzantine_inds=None, give_inits = 0,
@@ -102,15 +103,15 @@ class Agent:
         if isByzantine:
             #self.brain = randomActions
             self.brain = byz_policy
-        else: 
+        else:
             #self.brain = randomActions
             self.brain = honest_policy
-        self.actionSpace = getActionSpace(params, isByzantine, byzantine_inds, can_send_either_value=params['honest_can_send_either_value']) 
+        self.actionSpace = getActionSpace(params, isByzantine, byzantine_inds, can_send_either_value=params['honest_can_send_either_value'])
         if isByzantine:
-            init_val = params['null_message_val'] # doesnt need an init value 
+            init_val = params['null_message_val'] # doesnt need an init value
         elif type(give_inits) is not int:
             init_val = give_inits[agentID]
-        elif type(give_only_own_init) is not str: 
+        elif type(give_only_own_init) is not str:
             init_val = give_only_own_init
         else:
             init_val = np.random.choice(params['commit_vals'], 1)[0]
@@ -121,7 +122,7 @@ class Agent:
             for a in range(params['num_agents']):
                 if a == agentID:
                     continue
-                else: 
+                else:
                     initState.append(give_inits[a])
                 if params['use_PKI']: # need to set null values as nobody has recieved values from anybody else
                     initState += [params['null_message_val']]*(params['num_agents']-1)
@@ -136,9 +137,9 @@ class Agent:
         self.committed_value = False
 
     def chooseAction(self, oneHotStateMapper, temperature, device, forceCommit=False):
-   
-        # look at the current state and decide what action to take. 
-        oh = toOneHotState(self.state, oneHotStateMapper, device) # each column is one of the states. 
+
+        # look at the current state and decide what action to take.
+        oh = toOneHotState(self.state, oneHotStateMapper, device) # each column is one of the states.
         #making a decision:
         #self.action = self.brain(self.actionSpace)
         # expects softmax to be applied to network first.
@@ -147,61 +148,61 @@ class Agent:
         if forceCommit:
             if not self.isByzantine: #if it is an honest agent
                 commit_inds = [ ind for ind, a in enumerate(self.actionSpace) if 'commit' in a ]
-                logits = logits[commit_inds] # make it so that the only logits are those for committing. 
+                logits = logits[commit_inds] # make it so that the only logits are those for committing.
         #print('here are the logits after', logits)
         real_logprobs = torch.log(torch.nn.functional.softmax(logits, dim=0)) # currently not vectorized
-        #should be able to apply sampling without computing this twice... 
-        temperature_probs =  torch.nn.functional.softmax(logits/temperature, dim=0) 
-        action_ind = torch.multinomial(temperature_probs, 1, replacement=False) # returns 1 sample per row. 
+        #should be able to apply sampling without computing this twice...
+        temperature_probs =  torch.nn.functional.softmax(logits/temperature, dim=0)
+        action_ind = torch.multinomial(temperature_probs, 1, replacement=False) # returns 1 sample per row.
         action_logprob = real_logprobs[action_ind]
-        
+
         self.action = self.actionSpace[action_ind]
 
-        if forceCommit: # if forcecommit, convert the chosen action back to commit and action space relevance. 
+        if forceCommit: # if forcecommit, convert the chosen action back to commit and action space relevance.
             if not self.isByzantine:
                 self.action = self.actionSpace[ commit_inds[action_ind] ]
 
-        if 'commit' in self.action: # checking for a commit. 
+        if 'commit' in self.action: # checking for a commit.
             self.committed_value = int(self.action.split('_')[1])
 
         return self.action, action_logprob, action_ind.numpy()[0]
-            
+
 def updateStates(params, agent_list):
     #look at all agent actions and update the state of each to accomodate actions
     #print('agent list', agent_list)
     for reciever in agent_list:
         #print('reciever state going into the update', reciever.state)
-        new_state = [reciever.initVal] # keep track of the agent's initial value, 
+        new_state = [reciever.initVal] # keep track of the agent's initial value,
         #want to show to the NN each time
-        actor_ind = 1 # this is always relative to the reciever. 
+        actor_ind = 1 # this is always relative to the reciever.
         for actor in agent_list:
-            if actor == reciever: # check if agent committed. 
+            if actor == reciever: # check if agent committed.
                 continue
-            # dont need to check if committed a value as already will prevent 
+            # dont need to check if committed a value as already will prevent
             # from taking any actions other than no send.
             new_state.append( actionEffect(params, actor.action, actor.initVal, reciever.state[actor_ind], reciever.agentID) )
             actor_ind +=1
         reciever.state = new_state
 
-    # this internally resolves all of the agents and sets their new state. 
+    # this internally resolves all of the agents and sets their new state.
     # Now if PKI I need to add on to each state the states the other agents recieved
     if params['use_PKI']:
         list_of_new_states = [] # need to get new states for all before updating them
         for reciever in agent_list:
-            new_state = [reciever.initVal] # get the current state that has been set. 
-            actor_ind = 1 # relative indexing. 
+            new_state = [reciever.initVal] # get the current state that has been set.
+            actor_ind = 1 # relative indexing.
             for actor in agent_list:
-                if actor == reciever: # check if agent committed. 
+                if actor == reciever: # check if agent committed.
                     continue
-                
+
                 new_state.append(reciever.state[actor_ind]) # appending what we already have
                 new_state += actor.state[1:]
-                actor_ind +=1 # relative indexing. 
+                actor_ind +=1 # relative indexing.
             list_of_new_states.append(new_state)
-        
+
         for i, reciever in enumerate(agent_list): # actually update the states of each
-            reciever.state = list_of_new_states[i] 
-                
+            reciever.state = list_of_new_states[i]
+
 
 def actionEffect(params, action, init_val, actor_prev_action_result, receiver_id):
     # return the effects of a particular action
@@ -216,12 +217,15 @@ def actionEffect(params, action, init_val, actor_prev_action_result, receiver_id
         if params['honest_can_send_either_value']==False:
             if 'init' in action: # will only be true if honests not allowed to send a different value
                 return init_val
-        else: 
+        else:
             return int(action.split('_')[-1])
-    
+
     elif 'agent-'+str(receiver_id) in action:
-        # getting the first value of this. 
+        # getting the first value of this.
         return int(action.split('agent-'+str(receiver_id)+'_v-')[-1][0])
+
+	elif 'sample' in action:
+		#
 
     else:
         return params['null_message_val']
@@ -236,7 +240,7 @@ def savePlot(params, to_plot, label, experiment_name):
     plt.gcf().savefig(params['directory']+label+experiment_name+'.png', dpi=200)
 
 def initStatesandAgents(params, honest_policy, byz_policy):
-    
+
     # need to randomize to avoid honest players learning which is Byzantine
     # index of Byzantines
     byzantine_inds = np.random.choice(range(params['num_agents']), size=params['num_byzantine'] ,replace=False )
@@ -245,7 +249,7 @@ def initStatesandAgents(params, honest_policy, byz_policy):
     byzantine_list = []
     honest_list = []
 
-    # giving the honest init vals to the byzantine. need to decide all of them here. 
+    # giving the honest init vals to the byzantine. need to decide all of them here.
     give_inits = list(np.random.choice([0,1], params['num_agents']))
 
     #print(give_inits)
@@ -256,7 +260,7 @@ def initStatesandAgents(params, honest_policy, byz_policy):
             a = Agent(params, honest_policy, byz_policy, True, i, byzantine_inds, give_inits=give_inits)
             byzantine_list.append(a)
             agent_list.append(a)
-        else: 
+        else:
             a = Agent(params, honest_policy, byz_policy, False, i, give_only_own_init=give_inits[i])
             #print('honest', i, 'give init', type(give_inits[i]))
             honest_list.append(a) #give_inits=give_inits
@@ -273,22 +277,22 @@ def honestPartiesCommit(honest_list):
 
 def giveReward(params, honest_parties, trajectory):
     # checks to see if the honest parties have obtained both
-    # consistency and validity 
-    # returns honest then byzantine reward. 
+    # consistency and validity
+    # returns honest then byzantine reward.
 
     satisfied_constraints = False
     #penalty for the number of rounds
-    #num_rounds = len(trajectory[list(trajectory.keys())[0]]) # already iterating through the 
+    #num_rounds = len(trajectory[list(trajectory.keys())[0]]) # already iterating through the
 
-    # if they started by committing, then punish: 
-    # a more handholding version would be to encourage them to send the first round. 
-    '''no_send_true_value_first_penalty=np.array([0,0]) # may be able to remove this later. 
-    #default if they did nothing wrong. 
+    # if they started by committing, then punish:
+    # a more handholding version would be to encourage them to send the first round.
+    '''no_send_true_value_first_penalty=np.array([0,0]) # may be able to remove this later.
+    #default if they did nothing wrong.
     for key, trajectory_rounds in trajectory.items(): # going through the keys and their list of state, action, action prob pairs
         if 'Byz-False' in key: #only getting honest
-            if honest_can_send_either_value==False: # make the string be init rather than a value. 
+            if honest_can_send_either_value==False: # make the string be init rather than a value.
                 string_to_check = 'init'
-            else: 
+            else:
                 string_to_check = str(trajectory_rounds[0][1][0])
             if 'send_to_all-value_'+string_to_check != trajectory_rounds[0][2]: # getting the action from the first round
                 return dishonesty_violation + round_penalty_total, satisfied_constraints
@@ -310,17 +314,16 @@ def giveReward(params, honest_parties, trajectory):
 
     # checking validity
     if len(set(starting_values)) ==1:
-        # if they are all the same and they havent 
+        # if they are all the same and they havent
         # agreed on the same value, then return -1
-        if starting_values != com_values:   
+        if starting_values != com_values:
             return params['validity_violation'], satisfied_constraints
 
-    # want them to commit to the majority init value: 
+    # want them to commit to the majority init value:
     if params['num_byzantine']==0:
         majority_init_value = np.floor((sum(starting_values)/len(starting_values))+0.5)
-        if com_values[0] != majority_init_value: # as already made sure they were all the same value. 
+        if com_values[0] != majority_init_value: # as already made sure they were all the same value.
             return params['majority_violation'], satisfied_constraints
 
     satisfied_constraints=True
     return params['correct_commit'], satisfied_constraints
-
